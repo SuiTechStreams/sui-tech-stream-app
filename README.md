19a94c6cf393f09271d861fd4de84333e7ba3e8a
Prithwi <prithwi.online11@gmail.com>
2024-05-21 23:11:38 +0530

    FEAT:TabBAr Added

--- README.md
+++ README.md
@@ -14,6 +14,9 @@ pnpm dev
 bun dev
 ```
 
+For fetching video data from /db/videodb.json: run in terminal:
+```bash json-server --watch /db/videodb.json --port 4000```
+
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
 You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
---
+++ src/components/global/tabbar.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useState } from "react";
+
+export default function TabBar() {
+  const [activeTab, setActiveTab] = useState("all");
+  const [tabContent, setTabContent] = useState("All Videos");
+
+  const handleTabClick = (content: string, tab: string) => {
+    setTabContent(content);
+    setActiveTab(tab);
+  };
+
+  return (
+    <div className="">
+      <Tabs value={activeTab}>
+        <TabsList className="flex space-x-4 border-b-2 bg-gray-800 border-gray-300 px-4">
+          <TabsTrigger
+            value="all"
+            className="py-2 px-6 text-xl border-b-2 border-transparent hover:border-gray-500 focus:outline-none focus-visible:border-blue-500"
+            onClick={() => handleTabClick("All Videos", "all")}
+          >
+            All
+          </TabsTrigger>
+          <TabsTrigger
+            value="gaming"
+            className="py-2 px-6 text-xl border-b-2 border-transparent hover:border-gray-500 focus:outline-none focus-visible:border-blue-500"
+            onClick={() => handleTabClick("Gaming Videos", "gaming")}
+          >
+            Gaming
+          </TabsTrigger>
+          <TabsTrigger
+            value="music"
+            className="py-2 px-6 text-xl border-b-2 border-transparent hover:border-gray-500 focus:outline-none focus-visible:border-blue-500"
+            onClick={() => handleTabClick("Music Videos", "music")}
+          >
+            Music
+          </TabsTrigger>
+          <TabsTrigger
+            value="technology"
+            className="py-2 px-6 text-xl border-b-2 border-transparent hover:border-gray-500 focus:outline-none focus-visible:border-blue-500"
+            onClick={() => handleTabClick("Technology Videos", "technology")}
+          >
+            Technology
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="all">
+          {activeTab === "all" && tabContent}
+        </TabsContent>
+        <TabsContent value="gaming">
+          {activeTab === "gaming" && tabContent}
+        </TabsContent>
+        <TabsContent value="music">
+          {activeTab === "music" && tabContent}
+        </TabsContent>
+        <TabsContent value="technology">
+          {activeTab === "technology" && tabContent}
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+}
