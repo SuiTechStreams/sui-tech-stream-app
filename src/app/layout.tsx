4bdd7edd945e5c13f2d5f60f9906f4371c2c9411
Prithwi <prithwi.online11@gmail.com>
2024-05-31 07:10:52 +0530

    videocard fetch and title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,6 +80,14 @@ export default function RootLayout({
           >
 
         <html lang="en">
+        <head>
+          <link rel="icon" href="images/favicon.ico" sizes="any" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+          <link rel="manifest" href="/site.webmanifest"/>
+          <title>Chain Tube</title>
+        </head>
           <body className={inter.className}>
             <ThemeProvider>
               {/* Navbar */}
--- src/components/global/videocard.tsx
+++ src/components/global/videocard.tsx
@@ -12,26 +12,150 @@ import Image from "next/image";
 import Link from "next/link";
 import { Video } from "@/types";
 import React from "react";
+import { useState, useEffect } from "react";
 
 
 async function getVideos(): Promise<Video[]> {
-  const result = await fetch("http://localhost:4000/videos");
-  return result.json()
+  // const result = await fetch("http://localhost:4000/videos");
+  // return result.json()
+  return [
+    {
+      "id": 1,
+      "video_image": "video_1.jpg",
+      "title": "Move Smart Contract Language Tutorial for Aptos & Sui",
+      "channel_name": "PC Crypto",
+      "time": "6:52",
+      "views": "9k",
+      "uploaded_time": "1 year ago",
+      "link": "https://www.youtube.com/watch?v=dqWVBpvfcRw",
+      "channel_img": "channel_1.jpg"
+    },
+    {
+      "id": 2,
+      "video_image": "video_2.jpg",
+      "title": "Let’s get MOVING | Sui Blockchain",
+      "channel_name": "Sui Network",
+      "time": "37:11",
+      "views": "1.8K",
+      "uploaded_time": "1 year ago",
+      "link": "https://www.youtube.com/watch?v=b_2jZ4YEfWc",
+      "channel_img": "channel_2.jpg"
+    },
+    {
+      "id": 3,
+      "video_image": "video_3.jpg",
+      "title": "Fast and Reliable Smart Contracts on Sui with Move - Converge22",
+      "channel_name": "Circle",
+      "time": "47:35",
+      "views": "4.1K",
+      "uploaded_time": "1 year ago",
+      "link": "https://www.youtube.com/watch?v=SjfZraYZ9cE",
+      "channel_img": "channel_3.jpg"
+    },
+    {
+      "id": 4,
+      "video_image": "video_4.jpg",
+      "title": "Evan Cheng Talks Sui Blockchain @ Web Summit 2022",
+      "channel_name": "Sui Network",
+      "time": "18:22",
+      "views": "4.6K",
+      "uploaded_time": "1 year ago",
+      "link": "https://www.youtube.com/watch?v=lWg66640bYU",
+      "channel_img": "channel_2.jpg"
+    },
+    {
+      "id": 5,
+      "video_image": "video_5.jpg",
+      "title": "Interact with smart contracts using truffle console",
+      "channel_name": "Tech Spectrum",
+      "time": "18:33",
+      "views": "1.1K",
+      "uploaded_time": "1 year ago",
+      "link": "https://www.youtube.com/watch?v=QXo5t0gxUeI",
+      "channel_img": "channel_4.jpg"
+    },
+    {
+      "id": 6,
+      "video_image": "video_6.jpg",
+      "title": "Introduction to Sui Network and Move",
+      "channel_name": "Sui Network",
+      "time": "51:55",
+      "views": "1.7K",
+      "uploaded_time": "11 months ago",
+      "link": "http://localhost:4000/videos/6",
+      "channel_img": "channel_2.jpg"
+    },
+    {
+      "id": 7,
+      "video_image": "video_7.jpg",
+      "title": "Is Web3 all Hype? Top 10 Web 3.0 Questions & Answers",
+      "channel_name": "Fireship",
+      "time": "9:28",
+      "views": "919K",
+      "uploaded_time": "2 years ago",
+      "link": "https://www.youtube.com/watch?v=wHTcrmhskto",
+      "channel_img": "channel_5.jpg"
+    },
+    {
+      "id": 8,
+      "video_image": "video_8.jpg",
+      "title": "What is Web 3.0? - NEXT Generation INTERNET is HERE! | TechBar",
+      "channel_name": "TechBar",
+      "time": "9:46",
+      "views": "476K",
+      "uploaded_time": "2 years ago",
+      "link": "https://www.youtube.com/watch?v=PyphVdVeP0A",
+      "channel_img": "channel_6.jpg"
+    },
+    {
+      "id": 9,
+      "video_image": "video_9.jpg",
+      "title": "What is a Web3 wallet?",
+      "channel_name": "DappRadar",
+      "time": "1:42",
+      "views": "19K",
+      "uploaded_time": "2 years ago",
+      "link": "https://www.youtube.com/watch?v=XMnghGaSH4Y",
+      "channel_img": "channel_7.jpg"
+    },
+    {
+      "id": 10,
+      "video_image": "video_10.jpg",
+      "title": "WHAT IS SUI | SUI Network-THE FASTEST GROWING BLOCKCHAIN IN 2024",
+      "channel_name": "Crypto Never Sleeps",
+      "time": "11:28",
+      "views": "1.3K",
+      "uploaded_time": "3 months ago",
+      "link": "https://www.youtube.com/watch?v=TNFHMLjT1fQ&t=1s",
+      "channel_img": "channel_8.jpg"
+    }
+  ]
 }
 
 
 export default function VideoCard() {
-    const [videos, setVideo] = React.useState<any>();
+  //   const [videos, setVideo] = React.useState<any>();
 
-    const fetchAllVideos = React.useCallback(async () => {
+  //   const fetchAllVideos = React.useCallback(async () => {
+  //     const response = await getVideos();
+  //     setVideo(response);
+  //   }, []);
+
+
+  // React.useEffect(() => {
+  //   fetchAllVideos();
+  // });
+
+  const [videos, setVideos] = useState<Video[]>([]);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
       const response = await getVideos();
-      setVideo(response);
-    }, []);
+      setVideos(response);
+    };
+    fetchVideos();
+  }, []);
 
-
-  React.useEffect(() => {
-    fetchAllVideos();
-  });
 
   return (
     <div className="grid grid-cols-3 gap-2 ">
